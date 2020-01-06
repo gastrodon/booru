3692067463860cf2b963a905a22cbf236a358b2d
Zero <dakoolstwunn@gmail.com>
2020-01-05 21:07:06 -0500

    Replace indidivual tag list methods generics
    Use generic methods for getting lists of tags of a type. This approach
    is also used to get the count property of those tag lists

--- types.go
+++ types.go
@@ -16,46 +16,84 @@ type Post struct {
 	tag_string_copyright string `json:"tag_string_copyright"`
 	tag_string_general   string `json:"tag_string_general"`
 	tag_string_meta      string `json:"tag_string_meta"`
+	// tag counts
+	tag_count           uint `json:"tag_count"`
+	tag_count_artist    uint `json:"tag_count_artist"`
+	tag_count_character uint `json:"tag_count_character"`
+	tag_count_copyright uint `json:"tag_count_copyright"`
+	tag_count_general   uint `json:"tag_count_general"`
+	tag_count_meta      uint `json:"tag_count_meta"`
 }
 
 /*
- * A list of all tags on this post
+ * Get all of the tags of a type of this post
+ *
+ * artist: 		tags of the artists who made this post
+ * character: 	tags of the characters in this post
+ * copyright: 	tags of the source material for this post
+ * general: 	tags that describe the content of this post
+ * meta: 		tags that describe the properties of this post
+ * all: 		all tags of all types
  */
-func (post Post) Tags() (tags []string) {
-	return stirngs.Split(post.tag_string, " ")
+func (post Post) Tags(tag_type string) (tags []string) {
+	var splittable string
+	switch tag_type {
+	case "artist":
+		splittable = post.tag_string_artist
+		break
+	case "character":
+		splittable = post.tag_string_character
+		break
+	case "copyright":
+		splittable = post.tag_string_copyright
+		break
+	case "general":
+		splittable = post.tag_string_character
+		break
+	case "meta":
+		splittable = post.tag_string_meta
+		break
+	case "all":
+	default:
+		splittable = post.tag_string
+		break
+	}
+
+	tags = strings.Split(splittable, " ")
+	return
 }
 
 /*
- * A list of artist tags. These are the artists who drew this image
+ * Get the number of tags of a type of this post
+ *
+ * artist: 		tags of the artists who made this post
+ * character: 	tags of the characters in this post
+ * copyright: 	tags of the source material for this post
+ * general: 	tags that describe the content of this post
+ * meta: 		tags that describe the properties of this post
+ * all: 		all tags of all types
  */
-func (post Post) ArtistTags() (tags []string) {
-	return strings.Split(post.tag_string_artist, " ")
-}
-
-/*
- * A list of character tags. These are the characters who are in this post
- */
-func (post Post) CharacterTags() (tags []string) {
-	return strings.Split(post.tag_string_character, " ")
-}
-
-/*
- A list of copyright tags. These describe from what ip this post is based
-*/
-func (post Post) CopyrightTags() (tags []string) {
-	return strings.Split(post.tag_string_copyright, " ")
-}
-
-/*
- * A list of general tags. These usually describe the post's content
- */
-func (post Post) GeneralTags() (tags []string) {
-	return strings.Split(post.tag_string_general, " ")
-}
-
-/*
- * A list of meta tags. This includes tags such as highres and duplicate
- */
-func (post Post) MetaTags() (tags []string) {
-	return strings.Split(post.tag_string_meta, " ")
+func (post Post) TagsCount(tag_type string) (count uint) {
+	switch tag_type {
+	case "artist":
+		count = post.tag_count_artist
+		return
+	case "character":
+		count = post.tag_count_character
+		return
+	case "copyright":
+		count = post.tag_count_copyright
+		return
+	case "general":
+		count = post.tag_count_character
+		return
+	case "meta":
+		count = post.tag_count_meta
+		return
+	case "all":
+	default:
+		count = post.tag_count
+		return
+	}
+	return
 }
