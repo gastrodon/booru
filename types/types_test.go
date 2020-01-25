1f25758268cd24000d39a3e1a80efc3fc6efe8e2
Zero <dakoolstwunn@gmail.com>
2020-01-25 17:29:12 -0500

    add Get current timestamp in testmain
    Get the current timestamp in testmain so that other functions may use it

--- types/types_test.go
+++ types/types_test.go
@@ -3,12 +3,15 @@ package types
 import (
 	"os"
 	"testing"
+	"time"
 )
 
 var test_live Client
 var test_post Post
 var test_user User
 var test_pool Pool
+var test_profile Profile
+var now int64
 
 func TestMain(main *testing.M) {
 	test_live = Client{
@@ -17,5 +20,8 @@ func TestMain(main *testing.M) {
 	test_post, _, _ = test_live.GetPost(1)
 	test_user, _, _ = test_live.GetUser(581729)
 	test_pool, _, _ = test_live.GetPool(2)
+	test_profile, _, _ = test_auth.GetProfile()
+
+	now = time.Now().Unix()
 	os.Exit(main.Run())
 }
